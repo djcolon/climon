5ddcbc42885b48fda6c23e041decc9855eee4211
Daniel Colon <djcolon90@gmail.com>
2026-03-21 07:37:28 +0000

    Little tidy

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 // Libraries ///////////////////////////////////////////////////////////////////
 #include <Arduino.h>
 // Watchdog
-#include <esp_task_wdt.h>   
+#include <esp_task_wdt.h>
 // WiFi
 #include <WiFi.h>
 #include <DNSServer.h>
@@ -52,74 +52,87 @@ float pressure = 0.0;
 // Functions ///////////////////////////////////////////////////////////////////
 /**
  * Sets up mDNS so we have a name on the network.
-*/
-void setupMdns() {
-    //initialize mDNS service
-    esp_err_t err = mdns_init();
-    if (err) {
-        printf("MDNS Init failed: %d\n", err);
-        return;
-    }
+ */
+void setupMdns()
+{
+  // initialize mDNS service
+  esp_err_t err = mdns_init();
+  if (err)
+  {
+    printf("MDNS Init failed: %d\n", err);
+    return;
+  }
 
-    //set hostname
-    esp_err_t result = mdns_hostname_set(hostname.c_str());
-    if(result == ESP_OK) {
-      Serial.println("mdns_hostname_set success.");
-    }
-    else if(result == ESP_ERR_INVALID_ARG) {
-      Serial.println("mdns_hostname_set invalid arg.");
-    }
-    else if(result == ESP_ERR_NO_MEM) {
-      Serial.println("mdns_hostname_set no mem.");
-    }
-    //set default instance
-    result = mdns_instance_name_set(hostname.c_str());
-    if(result == ESP_OK) {
-      Serial.println("mdns_instance_name_set success.");
-    }
-    else if(result == ESP_ERR_INVALID_ARG) {
-      Serial.println("mdns_instance_name_set invalid arg.");
-    }
-    else if(result == ESP_ERR_NO_MEM) {
-      Serial.println("mdns_instance_name_set no mem.");
-    }
+  // set hostname
+  esp_err_t result = mdns_hostname_set(hostname.c_str());
+  if (result == ESP_OK)
+  {
+    Serial.println("mdns_hostname_set success.");
+  }
+  else if (result == ESP_ERR_INVALID_ARG)
+  {
+    Serial.println("mdns_hostname_set invalid arg.");
+  }
+  else if (result == ESP_ERR_NO_MEM)
+  {
+    Serial.println("mdns_hostname_set no mem.");
+  }
+  // set default instance
+  result = mdns_instance_name_set(hostname.c_str());
+  if (result == ESP_OK)
+  {
+    Serial.println("mdns_instance_name_set success.");
+  }
+  else if (result == ESP_ERR_INVALID_ARG)
+  {
+    Serial.println("mdns_instance_name_set invalid arg.");
+  }
+  else if (result == ESP_ERR_NO_MEM)
+  {
+    Serial.println("mdns_instance_name_set no mem.");
+  }
 }
 
 /**
  * Sets up the temp sensor.
  */
-void setupSensor() {
+void setupSensor()
+{
   // BME280
   Wire.begin(BME_SDA, BME_SCL);
   unsigned status;
-  
+
   // Init temp sensor.
   status = bme.begin(0x76, &Wire);
-  if (!status) {
-      Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-      Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
-      Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-      Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-      Serial.print("        ID of 0x60 represents a BME 280.\n");
-      Serial.print("        ID of 0x61 represents a BME 680.\n");
-      while (1) delay(10);
+  if (!status)
+  {
+    Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+    Serial.print("SensorID was: 0x");
+    Serial.println(bme.sensorID(), 16);
+    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("        ID of 0x60 represents a BME 280.\n");
+    Serial.print("        ID of 0x61 represents a BME 680.\n");
+    while (1)
+      delay(10);
   }
   // Sample infrequently to prevent self-heating causing overly high readings.
   bme.setSampling(
-    Adafruit_BME280::MODE_FORCED,
-    Adafruit_BME280::SAMPLING_X1,
-    Adafruit_BME280::SAMPLING_X1,
-    Adafruit_BME280::SAMPLING_X1,
-    Adafruit_BME280::FILTER_OFF
-  );
+      Adafruit_BME280::MODE_FORCED,
+      Adafruit_BME280::SAMPLING_X1,
+      Adafruit_BME280::SAMPLING_X1,
+      Adafruit_BME280::SAMPLING_X1,
+      Adafruit_BME280::FILTER_OFF);
 }
 
 /**
  * Set up our webserver.
  */
-void setupServer() {
+void setupServer()
+{
   // Respond with a json object containing measurements.
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) { 
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { 
     AsyncResponseStream *response = request->beginResponseStream("application/json");
     response->print("{");
     response->print("\"h\":\"" + hostname + "\",");
@@ -129,11 +142,9 @@ void setupServer() {
     response->printf("\"u\":%d,", uptime);
     response->printf("\"v\":\"%s\"", VERSION);
     response->print("}");
-    request->send(response);
-  }); 
-  server.onNotFound([](AsyncWebServerRequest *request){
-    request->send(404, "text/plain", "The princess is in another castle. Go away.");
-  });
+    request->send(response); });
+  server.onNotFound([](AsyncWebServerRequest *request)
+                    { request->send(404, "text/plain", "The princess is in another castle. Go away."); });
   server.begin();
 }
 
@@ -141,9 +152,10 @@ void setupServer() {
  * Tries to get hostname from preferences.
  * Returns default if not found.
  */
-void loadPreferences() {
+void loadPreferences()
+{
   Preferences preferences;
-  preferences.begin("cliMon", true);
+  preferences.begin(HOSTNAME, true);
   hostname = preferences.getString("hostname", HOSTNAME);
   Serial.println("Loaded hostname from preferences: '" + hostname + "'");
   preferences.end();
@@ -153,9 +165,10 @@ void loadPreferences() {
  * Tries to store hostname to preferences.
  * Returns default if not found.
  */
-void savePreferences() {
+void savePreferences()
+{
   Preferences preferences;
-  preferences.begin("cliMon", false);
+  preferences.begin(HOSTNAME, false);
   preferences.putString("hostname", hostname);
   Serial.println("Saved hostname to preferences: '" + hostname + "'");
   preferences.end();
@@ -164,9 +177,10 @@ void savePreferences() {
 /**
  * Removes preferences.
  */
-void clearPreferences() {
+void clearPreferences()
+{
   Preferences preferences;
-  preferences.begin("cliMon", false);
+  preferences.begin(HOSTNAME, false);
   preferences.clear();
   Serial.println("Cleared preferences.");
   preferences.end();
@@ -178,7 +192,8 @@ void clearPreferences() {
  * Prints configured params.
  * Validates them and stores them.
  */
-void saveParamsCallback () {
+void saveParamsCallback()
+{
   Serial.println("Get Params:");
   Serial.print(custom_hostname.getID());
   Serial.print(" : ");
@@ -188,26 +203,28 @@ void saveParamsCallback () {
   char hostname_param[40];
   strcpy(hostname_param, custom_hostname.getValue());
   // We set the default to be empty, so if it is not, set it and store it.
-  if((hostname_param != NULL) && (hostname_param[0] != '\0')) {
+  if ((hostname_param != NULL) && (hostname_param[0] != '\0'))
+  {
     hostname = String(hostname_param);
     savePreferences();
     // Then restart to properly init.
-    //delay(2000);
-    //ESP.restart();
+    // delay(2000);
+    // ESP.restart();
   }
 }
 
 /**
  * Called on boot.
-*/
-void setup() {
+ */
+void setup()
+{
   // Serial
   Serial.begin(115200);
 
   // Watchdog
   Serial.println("Configuring watchdog.");
-  esp_task_wdt_init(WDT_TIMEOUT, true); //enable panic so ESP32 restarts
-  esp_task_wdt_add(NULL); //add current thread to WDT watch
+  esp_task_wdt_init(WDT_TIMEOUT, true); // enable panic so ESP32 restarts
+  esp_task_wdt_add(NULL);               // add current thread to WDT watch
 
   // Pins
   pinMode(BUTTON_PIN, INPUT);
@@ -222,12 +239,14 @@ void setup() {
   // Local initialization. Once its business is done, there is no need to keep
   // it around.
 
-  if(digitalRead(BUTTON_PIN) == HIGH) {
+  if (digitalRead(BUTTON_PIN) == HIGH)
+  {
     Serial.println("Resetting wifi settings.");
     wm.resetSettings();
     clearPreferences();
     digitalWrite(INTERNAL_LED, LOW);
-    for(int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++)
+    {
       delay(250);
       digitalWrite(INTERNAL_LED, HIGH);
       delay(250);
@@ -242,12 +261,13 @@ void setup() {
   wm.addParameter(&custom_hostname);
   wm.setConfigPortalBlocking(false);
   wm.setSaveParamsCallback(saveParamsCallback);
-  if(!wm.autoConnect(HOSTNAME)) {
+  if (!wm.autoConnect(HOSTNAME))
+  {
     Serial.println("Failed to connect to wifi. Starting config portal.");
   }
 
   lastConnectionTry = millis();
-  
+
   // if you get here you have connected to the WiFi
   Serial.println("Connected to WiFi. Starting.");
   // Print out the IP.
@@ -264,7 +284,8 @@ void setup() {
 /**
  * Prints values read from sensor.
  */
-void printValues() {
+void printValues()
+{
   Serial.print("Temperature = ");
   Serial.print(temperature);
   Serial.println(" °C");
@@ -296,7 +317,8 @@ void printValues() {
 /**
  * Reads the sensor in forced mode, and stores values to globals.
  */
-void readSensor() {
+void readSensor()
+{
   bme.takeForcedMeasurement();
   // Temp in °C
   temperature = bme.readTemperature();
@@ -308,16 +330,19 @@ void readSensor() {
 
 /**
  * Main application loop.
-*/
-void loop() {
+ */
+void loop()
+{
   unsigned long currentMillis = millis();
 
   // If we're running the WifiManager config portal, process it here.
   wm.process();
 
-  if(WiFi.status() == WL_CONNECTED) {
+  if (WiFi.status() == WL_CONNECTED)
+  {
     // Reset last connection time if current millis overflows (every ~49 days)
-    if(currentMillis < lastConnectionTry) {
+    if (currentMillis < lastConnectionTry)
+    {
       lastConnectionTry = currentMillis - 1;
     }
     uptime = (currentMillis - lastConnectionTry) / 1000;
@@ -326,40 +351,46 @@ void loop() {
     printValues();
 
     // Do our values make sense?
-    if(
-      temperature < -30.0 ||
-      temperature > 60.0 ||
-      humidity < 1.0 ||
-      humidity > 100.0
-    ) {
+    if (
+        temperature < -30.0 ||
+        temperature > 60.0 ||
+        humidity < 1.0 ||
+        humidity > 100.0)
+    {
       Serial.println("Read unlikely values from sensor. Restarting...");
       ESP.restart();
     }
-    else {
+    else
+    {
       esp_task_wdt_reset();
     }
   }
   // if WiFi is down, try reconnecting
-  else if(currentMillis - lastConnectionTry >= CONNECTION_RETRY_INTERVAL) {
+  else if (currentMillis - lastConnectionTry >= CONNECTION_RETRY_INTERVAL)
+  {
     retry_count++;
     Serial.println("Reconnecting to WiFi...");
     WiFi.disconnect();
     WiFi.reconnect();
     lastConnectionTry = currentMillis;
-    if(retry_count >= MAX_RETRY_COUNT) {
+    if (retry_count >= MAX_RETRY_COUNT)
+    {
       // Don't actually need to do this I suppose.
       retry_count = 0;
       ESP.restart();
     }
-    else {
+    else
+    {
       esp_task_wdt_reset();
     }
   }
-  
-  if(wm.getConfigPortalActive()) {
+
+  if (wm.getConfigPortalActive())
+  {
     esp_task_wdt_reset();
   }
-  else {
+  else
+  {
     // Only delay our sensor reads if we're not running captive portal.
     delay(5000);
   }
